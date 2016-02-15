97e6c05d33eaf644810274efe88095dfc99fc03d
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-15 13:30:32 -0700

    Changed checkerboard to have a gradient style

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for( var i = 0; i < 63; i++){
   div1.style.width = "11.1%";
   div1.style.float = "left";
   div1.paddingBottom = "11.1%";
-  div1.style.backgroundColor = "red";
+  div1.style.backgroundColor = "#800000";
   div1.style.height = "150px";
   div1.className = "checkerSpace";
   div1.margin = "-10px 0px 0px -10px";
@@ -18,6 +18,19 @@ for( var i = 0; i < 63; i++){
 
 var spaces = document.getElementsByClassName("checkerSpace");
 
-for( var j = 0; j < spaces.length; j++ ){
-  if( j % 2 === 0 ) spaces[j].style.backgroundColor = "black";
+// for( var j = 0; j < spaces.length; j++ ){
+//   if( j % 2 === 0 ) spaces[j].style.backgroundColor = "#009973";
+// }
+
+var color1 = 9973;
+for( var k = 0; k < spaces.length; k++ ){
+  if( k % 2 === 0 ){
+    var color1String = "#00" + color1;
+    spaces[k].style.backgroundColor = color1String;
+    color1 -= 100;
+  } else {
+    var color2String = "#80" + color1;
+    spaces[k].style.backgroundColor = color2String;
+    color1 -= 100;
+  }
 }
