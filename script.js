3c9ee9e697ada96667fe2fcc5b14032d4817ebd7
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-15 11:48:45 -0700

    Added random color functionality

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Your JS goes here
 document.getElementsByTagName("body")[0].style.margin = "0px";
 
-for( var i = 0; i < 63; i++){
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+for( var i = 0; i < 63; i++ ){
   var div1 = document.createElement("div");
+  var random = getRandomInt(0, 16777216 ).toString(16);
+  var numstring = "#" + random;
 
   div1.style.width = "11.1%";
   div1.style.float = "left";
   div1.paddingBottom = "11.1%";
-  div1.style.backgroundColor = "red";
+  div1.style.backgroundColor = numstring;
   div1.style.height = "150px";
   div1.className = "checkerSpace";
   div1.margin = "-10px 0px 0px -10px";
-
   document.body.appendChild(div1);
-
 }
 
 var spaces = document.getElementsByClassName("checkerSpace");
-
-for( var j = 0; j < spaces.length; j++ ){
-  if( j % 2 === 0 ) spaces[j].style.backgroundColor = "black";
-}
